7c745776b19f3779bc8678fab44c63bf72a56e71
Tanisha Chaudhary <tanishach203@gmail.com>
2021-07-08 00:12:18 +0545

    component

---
+++ frontend/src/Component/chat.component.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+const socket = io.connect(process.env.REACT_APP_SOCKET_URL);
+export const ChatComponent = () => {
+  return <div>
+      
+  </div>;
+};
